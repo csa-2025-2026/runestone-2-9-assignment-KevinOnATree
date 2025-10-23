fbf569ee8fabf16931102059f675a5f76ffab5fc
KevinOnATree <kon100@student.hbuhsd.edu>
2025-10-23 21:36:48 +0000

    my brain kinda fried

--- Main.java
+++ Main.java
@@ -4,6 +4,31 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Input a Word and target: ");
+    String word = sc.nextLine();
+    int maxCount = 0;
+    String maxLetter = "";
+    String target = "";
+
+    for (int i2 = 0; i2 < word.length(); i2++) {
+
+    target = word.substring(i2, i2 + 1);
+    int count = 0;
+
+        for (int i = 0; i < word.length(); i++) {
+
+          String currentLetter = word.substring(i, i + 1);
+            if (currentLetter.equals(target))
+            {
+              count++;
+            }
+    }
+    if (count > maxCount) {
+      maxCount = count;
+      maxLetter = target;
+    }
+    }
+    System.out.println("The letter " + maxLetter + " appears the most amount of times.");
   }
 }
